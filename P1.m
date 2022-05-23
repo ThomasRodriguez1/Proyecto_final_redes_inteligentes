15b9b1597c60e0a1ff22b552a709ba3edc995264
ThomasRodriguez1 <tfrv.acnt4@gmail.com>
2022-05-23 13:44:41 -0500

    Variables
    Creadas por Arturo

---
+++ P1.m
@@ -0,0 +1,29 @@
+clear all;
+close all;
+clc;
+
+%definicion de variables duante la ventana de Backoff
+DIFS = 10e-3; % Espacio entre tramas diferencial 
+SIFS = 5e-3; % Espacio entre tramas (debido a que la Tx no es instantanea)
+durRTS = 11e-3; %ready to send
+durCTS = 11e-3; %confirmacion, listo para recibir paquetes
+durACK = 11e-3; %paquetes de control
+durDATA = 43e-3 ; %paquetes de datos (un solo paquete)
+sigma = 1e-3 ; %duracion de cada miniranura
+
+I = 7; % numero de grados
+k = 15; %Tamaño maximo del buffer
+Tao = 18; %Numero de ranuras sleep 
+
+W = [ 16, 32, 64, 128, 256]; %Maximo numero de miniranuras
+N = [ 5, 10, 15, 20]; %Numero de nodos por grado
+lanmbda = [ 0.0005, 0 .005, 0.03 ] ; % Tasa de generacion de paquetes por segundo
+tsim = 0; % tiempo de simulacion inicia en 0
+
+
+
+
+
+
+
+
